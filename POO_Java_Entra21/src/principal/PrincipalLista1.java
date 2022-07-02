f86914386b752bec145be9b1fdc5575f3eab37e7
guest01 <guest01@FLE207D14.alunos.sc.senac.br>
2022-07-01 21:31:33 -0300

    Lista01 de POO Java - Completa

--- POO_Java_Entra21/src/principal/PrincipalLista1.java
+++ POO_Java_Entra21/src/principal/PrincipalLista1.java
@@ -16,12 +16,13 @@ public class PrincipalLista1 {
 	
 	public static void main(String[] args) {
 		
-		double valorParaSacar = 1000;
+		double valorParaSacar = 250;
 		
 		//Desenvolva uma aplicação (uma classe PrincipalLista1, com método main) que efetue as seguintes operações:
 		//Crie uma conta chamada “conta1”, com agência 123-7, conta 12345-6 e saldo inicial de 300.
 		Conta_principalLista1 conta1 = new Conta_principalLista1("123-7", "12345-6", 300);
 		System.out.println("Saldo da conta: R$" + conta1.getSaldoConta());
+		
 		System.out.println("Solicitado o saque de R$" + valorParaSacar);
 		
 		//Efetue um saque de 250 e mostre o valor sacado na tela.
@@ -29,31 +30,32 @@ public class PrincipalLista1 {
 		System.out.println("Sacado R$" + valorSacadoDeFato);
 		System.out.println("Saldo da conta: R$" + conta1.getSaldoConta());
 		
-		
 		//Faça um depósito de 200.
-		System.out.println("");
+		System.out.println("Solicitado o depósito de R$200");
 		conta1.depositar(200);
-		System.out.println("");
+		System.out.println("Saldo da conta: R$" + conta1.getSaldoConta());
 		
 		//Efetue outro saque de 300 e mostre o valor sacado na tela.
+		System.out.println("Solicitado o saque de R$300");
+		System.out.println("Sacado R$" + conta1.sacar(300));
+		System.out.println("Saldo da conta: R$" + conta1.getSaldoConta());
 
-
-		
 		//Faça um depósito de 200.
-
+		System.out.println("Solicitado o depósito de R$200");
+		conta1.depositar(200);
+		System.out.println("Saldo da conta: R$" + conta1.getSaldoConta());
 	
-		
 		//Faça um rendimento de 0.5%.
-
+		System.out.println("Solicitado o rendimento de 0.5%");
+		conta1.efetuarRendimento(0.5);
+		System.out.println("Saldo da conta: R$" + conta1.getSaldoConta());
 	
-		
 		//Faça um rendimento de 0.8%.
+		System.out.println("Solicitado o rendimento de 0.8%");
+		conta1.efetuarRendimento(0.8);
+		System.out.println("Saldo da conta: R$" + conta1.getSaldoConta());
 
-
-	
 		//Mostre o valor do saldo atual na tela.
-
-	
-	
+		System.out.println("Saldo da conta: R$" + conta1.getSaldoConta());
 	}
 }
